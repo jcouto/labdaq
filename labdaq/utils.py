b57148ef9148ccbe296b0eb61e156a7e1c75616d
Joao Couto <jpcouto@gmail.com>
2020-07-25 14:48:44 -0400

    Draft of the general structure and default preferences.

---
+++ labdaq/utils.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+import time
+from datetime import datetime
+
+def display(msg):
+    sys.stdout.write('['+datetime.today().strftime('%y-%m-%d %H:%M:%S')+'] - ' + msg + '\n')
+    sys.stdout.flush()
+        
+default_preferences = dict(
+    channel_modes=dict(cc=dict(description='current clamp',
+                               input_units = 'pA',
+                               output_units = 'mV',
+                               input_conversion = 0.1,
+                               output_conversion = 0.01),
+                       vc=dict(description='voltage clamp',
+                               input_units = 'pA',
+                               output_units = 'mV',
+                               input_conversion = 0.1,
+                               output_conversion = 0.01),
+                       ttl = dict(description='TTL digital logic',
+                                  input_units = 'na',
+                                  output_units = 'na',
+                                  input_conversion = 1,
+                                  output_conversion = 1),
+                       analog = dict(description='regular_output',
+                                     input_units = 'V',
+                                     output_units = 'V',
+                                     input_conversion = 1,
+                                     output_conversion = 1)),
+    channels = [dict(name='ephys_in',
+                     type='analog_input',
+                     modes=['vc','cc'],
+                     device='ni:Dev0',
+                     channel='ai0',
+                     acq_rate=20000,
+                     range=[-10,10]),
+                dict(name='ephys_out',
+                     type='analog_output',
+                     modes=['vc','cc'],
+                     device='ni:Dev0',
+                     channel='ao0',
+                     acq_rate=20000,
+                     range=[-10,10])])
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Install script for labdaq.
+# Joao Couto - July 2020
+
+import os
+from os.path import join as pjoin
+from setuptools import setup
+from setuptools.command.install import install
+
+
+longdescription = ''' DAQ IO and simple ephys.'''
+data_path = pjoin(os.path.expanduser('~'), '.labdaq')
+
+setup(
+  name = 'labdaq',
+  version = '0.0',
+  author = 'Joao Couto',
+  author_email = 'jpcouto@gmail.com',
+  description = (longdescription),
+  long_description = longdescription,
+  license = 'GPL',
+  packages = ['labdaq'],
+  entry_points = {
+        'console_scripts': [
+          'labdaq = labdaq.cli:main',
+        ]
+        },
+    )
+
+
