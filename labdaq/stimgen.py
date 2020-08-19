fa5f0470d499c62587fa30c1ec684b1b237490f4
Joao Couto <jpcouto@gmail.com>
2020-08-19 11:00:38 -0400

    Added Square, Sawtooth, Chirp and Ramp waveforms. Added Exponent.

--- labdaq/stimgen.py
+++ labdaq/stimgen.py
@@ -1,4 +1,17 @@
 from .utils import *
+import scipy.signal as signal
+
+codes_doc = '''
+    
+    Implemented codes:
+       - 1 - DC waveform                | p1 - amp
+       - 2 - Ornstein-Uhlenbeck process | p1 - amp | p2 - sigma | p3 - tau
+       - 3 - Sine waveform              | p1 - amp | p2 - freq (Hz) | p3 - phase
+       - 4 - Square waveform            | p1 - amp | p2 - freq (Hz) | p3 - duty-cycle
+       - 5 - Sawtooth wave              | p1 - amp | p2 - freq (Hz) | p3 - duty-cycle
+       - 6 - Chirp waveform             | p1 - amp | p2 - freq start (Hz) | p3 - freq end (Hz)
+       - 7 - Ramp waveform              | p1 - start amp | p2 - end amp
+'''
 
 def stimgen_subwaveform(duration,
                         code,
@@ -10,17 +23,23 @@ def stimgen_subwaveform(duration,
                         fixseed=0,
                         seed=0,
                         operator=0,
+                        exponent = 1,
                         srate = 20000):
     '''
     This mimics some of the functionality of stimgen by Michele Giugliano and Maura Arsiero
     
-    For simplicity the subcode and the exponent fields were dropped.
+    For simplicity the subcode field was dropped.
     
     Implemented codes:
-       - 1 - DC waveform | p1 - amp
+       - 1 - DC waveform                | p1 - amp
        - 2 - Ornstein-Uhlenbeck process | p1 - amp | p2 - sigma | p3 - tau
-       - 3 - Sine waveform | p1 - amp | p2 - freq | p3 - phase
+       - 3 - Sine waveform              | p1 - amp | p2 - freq (Hz) | p3 - phase
+       - 4 - Square waveform            | p1 - amp | p2 - freq (Hz) | p3 - duty-cycle
+       - 5 - Sawtooth wave              | p1 - amp | p2 - freq (Hz) | p3 - duty-cycle
+       - 6 - Chirp waveform             | p1 - amp | p2 - freq start (Hz) | p3 - freq end (Hz) | p4 - method (linear,log,quadratic)
+       - 7 - Ramp waveform              | p1 - start amp | p2 - end amp
     '''
+
     N = int(duration*srate)
     if code == 1:
         # DC constant value waveform
@@ -43,47 +62,102 @@ def stimgen_subwaveform(duration,
         # Sine wave
         x = np.arange(N)
         swave = p1*np.sin(((p2*2*np.pi)/srate)*x + ((p3*2*np.pi)))
+    elif code == 4:
+        # Square waveform
+        t = np.arange(N)/srate
+        if p3 == 0:
+            p3 = 0.5
+        else:
+            p3 = np.clip(p3,0,1)
+        swave = p1*signal.square(p1*2*np.pi*t,duty=p3)
+    elif code == 5:
+        # Sawtooth waveform
+        t = np.arange(N)/srate
+        p3 = np.clip(p3,0,1)
+        swave = p1*signal.sawtooth(p1*2*np.pi*t,width=p3)
+    elif code == 6:
+        # Chirp waveform
+        t = np.arange(N)/srate
+        if p4 == 1:
+            method = 'logarithmic'
+        elif p4 == 2:
+            method = 'quadratic'
+        else:
+            method = 'linear'
+        swave = p1*signal.chirp(t,p2,t[-1],p3,method = method)
+    elif code == 7:
+        # Ramp waveform
+        t = np.arange(N)/srate
+        swave = np.linspace(p1,p2,N)
     else:
         raise ValueError('Code {0} - not implemented.'.format(code))
-    return swave
+    return np.power(swave,exponent)
 
 def stimgen_waveform(codes,srate = 20000):
     '''
-    Implemented codes:
-       - 1 - DC waveform | p1 - amp
-       - 2 - Ornstein-Uhlenbeck stochastic
-       - 3 - Sine waveform | p1 - amp | p2 - freq | p3 - phase
-    
+
+
     This mimics some of the functionality of stimgen by Michele Giugliano and Maura Arsiero
     
-    For simplicity the subcode and the exponent fields were dropped.
-    Example:
+    For simplicity the subcode field was dropped.
+
+See also: 
+
+stimgen_subwaveform(duration,
+                        code,
+                        p1=0,
+                        p2=0,
+                        p3=0,
+                        p4=0,
+                        p5=0,
+                        fixseed=0,
+                        seed=0,
+                        operator=0,
+                        exponent = 1,
+                        srate = 20000)
+
+Implemented codes:
+
+       - 1 - DC waveform                | p1 - amp
+       - 2 - Ornstein-Uhlenbeck process | p1 - amp | p2 - sigma | p3 - tau
+       - 3 - Sine waveform              | p1 - amp | p2 - freq (Hz) | p3 - phase
+       - 4 - Square waveform            | p1 - amp | p2 - freq (Hz) | p3 - duty-cycle
+       - 5 - Sawtooth wave              | p1 - amp | p2 - freq (Hz) | p3 - duty-cycle
+       - 6 - Chirp waveform             | p1 - amp | p2 - freq start (Hz) | p3 - freq end (Hz)
+       - 7 - Ramp waveform              | p1 - start amp | p2 - end amp    
+    
+
+Example:
 
 import pylab as plt
-plt.plot(stimgen_waveform([[1,1,0,10,1,0,0,0,0,0],
-                           [1,3,5,5,0.75,0,0,0,0,0],
+plt.plot(stimgen_waveform([[1,1,0],
+                           [1,3,5,5,0.75],
                            [1,1,10,0,0,0,0,0,1,1],
-                          [1,1,0,10,1,0,0,0,0,0]]))    
+                           [1,1,0]]),srate = sampling_rate)    
     '''
+
     lastwave = None
     waves = []
     for c in codes:
-        assert len(c) == 10, "Waveform codes need 10 entries."
-        if not lastwave is None and c[9]>0:
-            wave = stimgen_subwaveform(len(lastwave)/srate,*c[1:],
-                                       srate = srate) 
+        operator = 0
+        if not lastwave is None and len(c)>=10:
+            if c[9]>0: # to use the time from the last wave
+                wave = stimgen_subwaveform(len(lastwave)/srate,*c[1:],
+                                           srate = srate) 
         else:
             wave = stimgen_subwaveform(*c,srate = srate)
-        if not lastwave is None and c[9]>0:
-            if c[9] == 1:
+        
+        if not lastwave is None and len(c)>=10:
+            operator = c[9]
+            if operator == 1:
                 lastwave += wave
-            elif c[9] == 2:
+            elif operator == 2:
                 lastwave *= wave
-            elif c[9] == 3:
+            elif operator == 3:
                 lastwave -= wave
-            elif c[9] == 4:
+            elif operator == 4:
                 lastwave /= wave
-            else:
+            elif not operator == 0:
                 raise ValueError('Unknown code for stimgen operator.')
             wave = lastwave
         else:
--- labdaq/utils.py
+++ labdaq/utils.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 import numpy as np
 import time
 from datetime import datetime
+import sys
+import os
+from os.path import join as pjoin
 
 def display(msg):
     sys.stdout.write('['+datetime.today().strftime('%y-%m-%d %H:%M:%S')+'] - ' + msg + '\n')
@@ -59,4 +62,47 @@ default_preferences = dict(
                      device='ni:Dev2',
                      type='axon200B_mode',
                      modes=['analog'],
-                     channel = 'ai1')])
+                     channel = 'ai1')],
+    recorder = dict(path = 'C:\\data\\{subject}\\ephys\\{datetime}_{subject}',
+                    compress = True,
+                    format = 'h5'))
+
+preferencepath = pjoin(os.path.expanduser('~'), 'labdaq')
+
+def get_preferences(user = 'default', create = True, homedir = preferencepath):
+    '''
+    Reads the preference file from the home directory
+
+    prefs = get_preferences('username')
+
+    Example user parameters ( for patch clamping with an AXON 700B):
+    
+    {0}
+    '''.format(default_preferences)
+
+    preffilepath = pjoin(homedir,user,'prefs.json')
+    if not os.path.isfile(preffilepath) and create:
+        display('Creating preference file from defaults.')
+        prefpath = os.path.dirname(preffilepath)
+        if not os.path.isdir(prefpath):
+            os.makedirs(prefpath)
+            os.makedirs(pjoin(prefpath,'protocols'))
+            
+        with open(preffilepath, 'w') as outfile:
+            json.dump(default_preferences, outfile, sort_keys = True, indent = 4)
+            display('Saving default preferences to: ' + preffilepath)
+            print('''
+
+             >>> Edit the file before launching. <<<
+
+            ''')
+            sys.exit(0)
+
+    if os.path.isfile(preffilepath):
+        with open(preffilepath, 'r') as infile:
+            pref = json.load(infile)
+        # TODO: This should check that all fields are there by comparing with default_preferences
+    return pref
+
+
+    
